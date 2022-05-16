2213933630293bf4b5b7804b6690b0114df126b3
Leon Cheng <leonc@mit.edu>
2022-05-16 04:40:35 -0400

    wip: starting to style cards

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 ## Next TODOs
 
+### Core
+
 - fonts
 - next component (contact, sections, cards)
 - style all the a href links to something more minimalistic
+
+### Advanced
+
+- dark mode
+- mobile friendly
--- components/experiences/experiences.tsx
+++ components/experiences/experiences.tsx
@@ -3,10 +3,26 @@ import styles from "./experiences.module.css";
 
 const Experiences = () => {
   return (
-    <div className={styles.container}>
-      <p className={classNames(styles.sectionTitle, "GLOBALS-section")}>
-        Company
-      </p>
+    <div className={classNames(styles.container, "GLOBALS-section")}>
+      <p className={styles.sectionTitle}>Company</p>
+
+      <div className={styles.cardsWrapper}>
+        <div className={styles.card}>
+          <p>Summary Phrase</p>
+          <p>
+            text text text text text text text text text text text text text
+            text text text{" "}
+          </p>
+        </div>
+
+        <div className={styles.card}>
+          <p>Summary Phrase</p>
+          <p>
+            text text text text text text text text text text text text text
+            text text text{" "}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
