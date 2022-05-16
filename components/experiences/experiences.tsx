c822dc526c13add314f3875ba4cfae8f1cbb615e
Leon Cheng <leonc@mit.edu>
2022-05-16 04:17:47 -0400

    section title

--- components/experiences/experiences.tsx
+++ components/experiences/experiences.tsx
@@ -1,7 +1,14 @@
+import classNames from "classnames";
 import styles from "./experiences.module.css";
 
 const Experiences = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <p className={classNames(styles.sectionTitle, "GLOBALS-section")}>
+        Company
+      </p>
+    </div>
+  );
 };
 
 export default Experiences;
