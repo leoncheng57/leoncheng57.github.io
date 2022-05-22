48a99338a8ec002ba28d5dd67ce123fca1ce7739
Leon Cheng <leonc@mit.edu>
2022-05-22 00:51:19 -0400

    fix assetPath for css, image src paths to local (#15)

---
+++ docs/_next/static/lzw1N4GHQHo38OmSrfnIl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/30daa2465978fe2e.css","static/chunks/pages/index-bb1eac4a364c9d8e.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
