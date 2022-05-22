9b2734001f24ec3707688e079162294f1798c6c8
Leon Cheng <leonc@mit.edu>
2022-05-22 16:40:33 -0400

    Fix reload nojekyll (#18)
    * add .nojekyll file on each build-export
    
    * hot reload seems fixed, assetPrefix should be conditional on local or prod env
    
    * got .env.production working

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const isProd = process.env.IS_PROD;
+console.log("*********************");
+console.log("inside next.config.js");
+console.log("isProd: ", isProd);
+console.log("*********************");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: `./`,
+  assetPrefix: isProd ? `./` : "",
 };
 
 module.exports = nextConfig;
