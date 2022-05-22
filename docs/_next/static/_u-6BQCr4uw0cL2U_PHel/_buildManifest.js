4d901459857c6b91af4b977e2f03ecd41ff3f760
Leon Cheng <leonc@mit.edu>
2022-05-22 16:08:27 -0400

    run build-export

--- docs/_next/static/_u-6BQCr4uw0cL2U_PHel/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/30daa2465978fe2e.css","static/chunks/pages/index-5765c1781134a6f6.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
