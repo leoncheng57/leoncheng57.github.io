90ae499ff43bd2ad1948ecae4a1cf6d850706f9f
Leon Cheng <leonc@mit.edu>
2022-05-22 21:14:03 -0400

    Add activities details (#19)
    * try to fix favicon path
    
    * add activities details
    
    * TODOS.md file
    
    * more activities details
    
    * add more images
    
    * css scroll behavior smooth
    
    * more details
    
    * finish details for all cards
    
    * periods
    
    * update todos, build-export

---
+++ TODOS.md
@@ -0,0 +1,12 @@
+# Next TODOs
+
+### Core
+
+- add google analytics
+- replace old website with this website
+
+### Future
+
+- add a blog section
+- dark mode
+- top nav / footer
--- components/experiences/activities/activities.tsx
+++ components/experiences/activities/activities.tsx
@@ -8,53 +8,163 @@ const Activities = () => {
 
       <div className={sharedStyles.cardsWrapper}>
         <Card
-          summaryText={`Upstyling Nonprofit Design & Website `}
-          details={`blah blah blah blah blah blah blah blah blah`}
+          summaryText={`Stuyvesant Mentoring Program`}
+          details={
+            <>
+              Remotely mentor high school students. Focus on advising for
+              long-term career success and personal happiness.
+            </>
+          }
           image={
             <img
-              alt="upstlying project image"
+              alt="stuyvesant mentoring project image"
+              src="./imagesActivities/stuyvesant.jpg"
+            />
+          }
+        />
+
+        <Card
+          summaryText={`Upstyling Nonprofit Design & Website `}
+          details={
+            <>
+              Collaborated with nonprofit to develop a brand website. See Adobe
+              XD mockups for{" "}
+              <a href="https://xd.adobe.com/view/ccdb2cc6-62bd-4255-9323-0965bdc5663a-9988/">
+                Mobile
+              </a>{" "}
+              and{" "}
+              <a href="https://xd.adobe.com/view/39493f1d-21c8-403f-a85d-62ea41079bb4-6c1a/">
+                Desktop
+              </a>
+              , as well as public website at{" "}
+              <a href="https://upstyling.github.io/">upstyling.github.io</a>.
+            </>
+          }
+          image={
+            <img
+              alt="upstyling project image"
               src="./imagesActivities/upstyling.png"
             />
           }
         />
 
         <Card
-          summaryText={`Solstice Fayemz Website Design `}
-          details={`blah blah blah blah blah blah blah blah blah`}
+          summaryText={`Solstice Fayemz Design Mockup`}
+          details={
+            <>
+              Collaborated with the singer/songwriter to develop a website
+              design that conveyed their taste and passion. You can view the
+              Adobe XD mockup{" "}
+              <a href="https://xd.adobe.com/view/7dc95ca3-6a53-40a6-b813-4c7250d6e2e9-289a/screen/52df4fba-9a19-47a6-bc26-5fb9a2f1a725/">
+                here
+              </a>
+              .
+            </>
+          }
           image={
             <img
-              alt="upstlying project image"
-              src="https://leoncheng.dev/images/solstice.png"
+              alt="solstice project image"
+              src="./imagesActivities/solstice.png"
             />
           }
         />
 
         <Card
-          summaryText={`Maynooth Practice Mockup `}
-          details={`blah blah blah blah blah blah blah blah blah`}
+          summaryText={`MIT Teaching Assistant`}
+          details={
+            <>
+              TA for Interconnected Embedded Systems and Computation Structures
+              courses.
+            </>
+          }
+          image={
+            <img
+              alt="mit teaching assistant image"
+              src="./imagesActivities/mit.png"
+            />
+          }
         />
 
         <Card
-          summaryText={`Adapty Floaty Prototype Device`}
-          details={`blah blah blah blah blah blah blah blah blah`}
+          summaryText={`StartLabs Club Website`}
+          details={
+            <>
+              Led the website development department to build a new site using
+              Flask, FastCGI, and JS. Designed by the design department. See
+              current site at{" "}
+              <a href="http://startup.mit.edu/">startup.mit.edu</a>.
+            </>
+          }
+          image={
+            <img
+              alt="startlabs club image"
+              src="./imagesActivities/startlabs.png"
+            />
+          }
         />
 
         <Card
-          summaryText={`MIT CETI`}
-          details={`blah blah blah blah blah blah blah blah blah`}
+          summaryText={`Adapty Floaty Prototype Assistive Floatation Device Project`}
+          details={
+            <>
+              Worked in a team of 2 MIT students to build an interactive device
+              that can intelligently adapt to the user&#39;s needs. Device will
+              inflate as you sink deeper and deflate as you float higher, to be
+              used for swim training. See more{" "}
+              <a href="https://leoncheng.dev/Adapty-Floaty/">here</a>.
+            </>
+          }
+          image={
+            <img
+              alt="adapty floaty project image"
+              src="./imagesActivities/adaptyfloaty.png"
+            />
+          }
+        />
+
+        <Card
+          summaryText={`CETI Teaching & Leadership Program`}
+          details={
+            <>
+              Created and taught lessons on web development and python
+              programming to local students in Asia. See some lessons{" "}
+              <a href="https://leoncheng.dev/MYCamp-Web-Dev-Lessons/">here</a>.
+            </>
+          }
           image={
             <img alt="ceti project image" src="./imagesActivities/ceti.jpg" />
           }
         />
 
         <Card
-          summaryText={`MIT-SUTD GLP`}
-          details={`blah blah blah blah blah blah blah blah blah`}
+          summaryText={`MIT-SUTD Global Leadership Program`}
+          details={
+            <>
+              Learned how to build a full electric boat using power tools,
+              computer aided design, and other fabrication materials in a team
+              with students from Singapore. Short video{" "}
+              <a href="https://www.youtube.com/watch?v=-eT_rZoE87s&ab_channel=JunWenLoo">
+                here
+              </a>
+              .
+            </>
+          }
+          image={
+            <img alt="glp project image" src="./imagesActivities/glp.png" />
+          }
         />
 
         <Card
-          summaryText={`StuyHacks Hackathon`}
-          details={`blah blah blah blah blah blah blah blah blah`}
+          summaryText={`Organizer for StuyHacks Hackathon`}
+          details={
+            <>
+              Recruited company sponsors and organized prizes to help make the
+              fun high school hackathon possible.
+            </>
+          }
+          image={
+            <img alt="stuyhacks image" src="./imagesActivities/stuyhacks.jpg" />
+          }
         />
       </div>
     </div>
