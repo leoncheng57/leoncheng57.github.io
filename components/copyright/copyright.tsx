0bf75553b8bd28d5443b2da7a49fc1e18ce08111
Leon Cheng <leonc@mit.edu>
2022-06-09 23:24:11 -0400

    Add logo (#22)
    * update todos
    
    * add logo to copyright section

--- components/copyright/copyright.tsx
+++ components/copyright/copyright.tsx
@@ -4,6 +4,9 @@ import styles from "./copyright.module.css";
 const Copyright: NextPage = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <img alt="upstyling project image" src="./lc-logo.png" />
+      </div>
       <p>© Copyright 2022 Leon Cheng</p>
       <p>
         Designed with Figma. Built with TypeScript, React, NextJS. No design
