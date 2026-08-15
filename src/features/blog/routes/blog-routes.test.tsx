21362657787ee9052ce8c1b3818a8e1d35ed3523
Leon Cheng <leon.cheng57@gmail.com>
2026-08-15 13:29:38 -0400

    blog: add House Party Photo Hunt article (#200)

--- src/features/blog/routes/blog-routes.test.tsx
+++ src/features/blog/routes/blog-routes.test.tsx
@@ -26,9 +26,9 @@ describe('blog routes', () => {
     ).toHaveAttribute('href', '/guides/manager-worker-parallel-agents')
     expect(
       screen.getByRole('link', {
-        name: /Worktrees, Remote Coding Agents, and Choosing the Right Kind of Isolation/i,
+        name: /Building House Party Photo Hunt/i,
       })
-    ).toHaveAttribute('href', '/blog/worktrees-vs-remote-coding-agents')
+    ).toHaveAttribute('href', '/blog/building-house-party-photo-hunt')
   })
 
   it('renders the blog index route', () => {
